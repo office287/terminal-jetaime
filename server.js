f6073d8dc272b27db0c0444752ec607bff8d4b82
office287 <office@amaliatradingroutes.ltd>
2026-04-13 21:07:47 +0300

    Add morgan request logging

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
